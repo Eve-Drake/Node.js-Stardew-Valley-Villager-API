153226ccb32c16905f29f43669fc044187c41f67
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-13 13:57:51 +0100

    More requests methods

--- index.js
+++ index.js
@@ -5,73 +5,83 @@ const app = express();
 app.use(cors())
 
 const charecters = [
-    {id: 0, name : 'Alex', Marriage: true, topGifts: ['Complete Breakfast', 'Salmon Dinner', 'Pearl'], Address: '1 River Road', Birthday: 'Summer: 13'},
-    {id: 1, name : 'Elliot', Marriage: true, topGifts: ['Lobster', 'Duck Feather', 'Squid Ink'], Address: 'Elliot\'s Cabin', Birthday: 'Fall: 5'},
-    {id: 2, name : 'Harvey', Marriage: true, topGifts: ['Coffee', 'Truffle Oil', 'Wine'], Address: 'Medical Clinic', Birthday: 'Winter: 14'},
-    {id: 3, name : 'Sam', Marriage: true, topGifts: ['Cactus Fruit', 'Maple Bar', 'Pizza'], Address: '1 Willow Lane', Birthday: 'Summer: 17'},
-    {id: 4, name : 'Sebastian', Marriage: true, topGifts: ['Frozen Tear', 'Sashimi', 'Obsidian'], Address: '24 Mountain Road', Birthday: 'Winter: 10'},
-    {id: 5, name : 'Shane', Marriage: true, topGifts: ['Beer', 'Pizza', 'Hot Pepper'], Address: 'Marnies Ranch', Birthday: 'Spring: 20'},
-    {id: 6, name : 'Abigail', Marriage: true, topGifts: ['Amathyst', 'Pumpkin', 'Pufferfish'], Address: 'Pierres General Store', Birthday: 'Fall 13'},
-    {id: 7, name : 'Emily', Marriage: true, topGifts: ['Cloth', 'Jade', 'Wool'], Address: '2 Willow Lane', Birthday: 'Spring 27'},
-    {id: 8, name : 'Haley', Marriage: true, topGifts: ['Pink Cake', 'Sunflower', 'Fruit Salad'], Address: '2 Willow Lane', Birthday: 'Spring 14'},
-    {id: 9, name : 'Leah', Marriage: true, topGifts: ['Salad', 'Wine', 'Truffle'], Address: 'Leahs Cottage', Birthday: 'Winter 23'},
-    {id: 10, name : 'Maru', Marriage: true, topGifts: ['Battery Pack', 'Diamond', 'Gold Bar'], Address: '24 Mountain Road', Birthday: 'Summer 10'},
-    {id: 11, name : 'Penny', Marriage: true, topGifts: ['Emerald', 'Poppy', 'Melon'], Address: 'Trailer', Birthday: 'Fall 2'},
-    {id: 12, name : 'Caroline', Marriage: false, topGifts: ['Green Tea', 'Summer Spangle', 'Fish Taco'], Address: 'Pierres General Store', Birthday: 'Winter 7'},
-    {id: 13, name : 'Clint', Marriage: false, topGifts: ['Emerald', 'Gold Bar', 'Ruby'], Address: 'Blacksmith', Birthday: 'Winter 26'},
-    {id: 14, name : 'Demitrius', Marriage: false, topGifts: ['Strawberry', 'Bean Hotpot', 'Ice Cream'], Address: '24 Mountain Road', Birthday: 'Summer 19'},
-    {id: 15, name : 'Dwarf', Marriage: false, topGifts: ['Emerald', 'Jade', 'Ruby'], Address: 'Eastern Cave', Birthday: 'Summer 22'},
-    {id: 16, name : 'Evelyn', Marriage: false, topGifts: ['Beet', 'Tulip', 'Diamond'], Address: '1 River Road', Birthday: 'Winter 20'},
-    {id: 17, name : 'George', Marriage: false, topGifts: ['Leek', 'Fried Mushroom', 'Pearl'], Address: '1 River Road', Birthday: 'Fall 24'},
-    {id: 18, name : 'Gus', Marriage: false, topGifts: ['Orange', 'Escargot', 'Fish Taco'], Address: 'The Stardrop Saloon', Birthday: 'Summer 8'},
-    {id: 19, name : 'Jas', Marriage: false, topGifts: ['Fairy Rose', 'Plum Pudding', 'Pink Cake'], Address: 'Marnies Ranch', Birthday: 'Summer 4'},
-    {id: 20, name : 'Jodi', Marriage: false, topGifts: ['Fried Eel', 'Chocolate Cake', 'Diamond'], Address: '1 Willow Lane', Birthday: 'Fall 11'},
-    {id: 21, name : 'Kent', Marriage: false, topGifts: ['Pearl', 'Fiddlehead Risotto', 'Roasted Hazelnuts'], Address: '1 Willow Lane', Birthday: 'Spring 4'},
-    {id: 22, name : 'Krobus', Marriage: false, topGifts: ['Diamond', 'Void Egg', 'Pumpkin'], Address: 'The Sewers', Birthday: 'Winter 1'},
-    {id: 23, name : 'Leo', Marriage: false, topGifts: ['Poi', 'Duck Feather', 'Mango'], Address: 'Ginger Island Forest', Birthday: 'Summer 26'},
-    {id: 24, name : 'Lewis', Marriage: false, topGifts: ['Hot Pepper', 'Green Tea', 'Glazed Yam'], Address: 'Mayors Manor', Birthday: 'Spring 7'},
-    {id: 25, name : 'Linus', Marriage: false, topGifts: ['Yam', 'Coconut', 'Blueberry Tart'], Address: 'Tent', Birthday: 'Winter 3'},
-    {id: 26, name : 'Marnie', Marriage: false, topGifts: ['Pink Cake', 'Pumpkin Pie', 'Diamond'], Address: 'Cindersnap Forest', Birthday: 'Fall 18'},
-    {id: 27, name : 'Pam', Marriage: false, topGifts: ['Beer', 'Mead', 'Parsnip'], Address: 'Trailer', Birthday: 'Spring 18'},
-    {id: 28, name : 'Pierre', Marriage: false, topGifts: ['Fried Calamari', 'Pearl', 'Rabbits Foot'], Address: 'Pierres General Store', Birthday: 'Spring 26'},
-    {id: 29, name : 'Robin', Marriage: false, topGifts: ['Goat Cheese', 'Peach', 'Spaghetti'], Address: '24 Mountain Road', Birthday: 'Fall 21'},
-    {id: 30, name : 'Sandy', Marriage: false, topGifts: ['Crocus', 'Daffodil', 'Sweet Pea'], Address: 'Oasis', Birthday: 'Fall 15'},
-    {id: 31, name : 'Vincent', Marriage: false, topGifts: ['Grape', 'Snail', 'Pink Cake'], Address: '1 Willow Lane', Birthday: 'Spring 10'},
-    {id: 32, name : 'Willy', Marriage: false, topGifts: ['Diamond', 'Mead', 'Octopus'], Address: 'Fish Shop', Birthday: 'Summer 24'},
-    {id: 33, name : 'Wizard', Marriage: false, topGifts: ['Purple Mushroom', 'Solar Essence', 'Void Essence'], Address: 'Wizards Tower', Birthday: 'Winter 17'},
-    {id: 34, name : 'Birdie', Marriage: false, Address: 'Ginder Island Beach Hut'},
-    {id: 35, name : 'Bouncer', Marriage: false, Address: 'Oasis'},
-    {id: 36, name : 'Gil', Marriage: false, Address: 'The Adventures Guild',},
-    {id: 37, name : 'Governor', Marriage: false},
-    {id: 38, name : 'Gunther', Marriage: false, Address: 'Museum'},
-    {id: 39, name : 'Henchman', Marriage: false, topGifts: 'Void Mayonnaise', Address: 'Witches Swamp'},
-    {id: 40, name : 'Morris', Marriage: false, Address: 'JojaMart'},
-    {id: 41, name : 'Marlon', Marriage: false, Address: 'The Adventures Guild',},
-    {id: 42, name : 'Mr. Qi', Marriage: false, Address: 'Casino or Walnut Room',},
-    {id: 43, name : 'Proffessor Snail', Marriage: false, Address: 'Island Field Office',},
+    {id: 0, name : 'Alex', marriage: true, topGifts: ['Complete Breakfast', 'Salmon Dinner', 'Pearl'], address: '1 River Road', birthday: 'Summer: 13'},
+    {id: 1, name : 'Elliot', marriage: true, topGifts: ['Lobster', 'Duck Feather', 'Squid Ink'], address: 'Elliot\'s Cabin', birthday: 'Fall: 5'},
+    {id: 2, name : 'Harvey', marriage: true, topGifts: ['Coffee', 'Truffle Oil', 'Wine'], address: 'Medical Clinic', birthday: 'Winter: 14'},
+    {id: 3, name : 'Sam', marriage: true, topGifts: ['Cactus Fruit', 'Maple Bar', 'Pizza'], address: '1 Willow Lane', birthday: 'Summer: 17'},
+    {id: 4, name : 'Sebastian', marriage: true, topGifts: ['Frozen Tear', 'Sashimi', 'Obsidian'], address: '24 Mountain Road', birthday: 'Winter: 10'},
+    {id: 5, name : 'Shane', marriage: true, topGifts: ['Beer', 'Pizza', 'Hot Pepper'], address: 'Marnies Ranch', birthday: 'Spring: 20'},
+    {id: 6, name : 'Abigail', marriage: true, topGifts: ['Amathyst', 'Pumpkin', 'Pufferfish'], address: 'Pierres General Store', birthday: 'Fall 13'},
+    {id: 7, name : 'Emily', marriage: true, topGifts: ['Cloth', 'Jade', 'Wool'], address: '2 Willow Lane', birthday: 'Spring 27'},
+    {id: 8, name : 'Haley', marriage: true, topGifts: ['Pink Cake', 'Sunflower', 'Fruit Salad'], address: '2 Willow Lane', birthday: 'Spring 14'},
+    {id: 9, name : 'Leah', marriage: true, topGifts: ['Salad', 'Wine', 'Truffle'], address: 'Leahs Cottage', birthday: 'Winter 23'},
+    {id: 10, name : 'Maru', marriage: true, topGifts: ['Battery Pack', 'Diamond', 'Gold Bar'], address: '24 Mountain Road', birthday: 'Summer 10'},
+    {id: 11, name : 'Penny', marriage: true, topGifts: ['Emerald', 'Poppy', 'Melon'], address: 'Trailer', birthday: 'Fall 2'},
+    {id: 12, name : 'Caroline', marriage: false, topGifts: ['Green Tea', 'Summer Spangle', 'Fish Taco'], address: 'Pierres General Store', birthday: 'Winter 7'},
+    {id: 13, name : 'Clint', marriage: false, topGifts: ['Emerald', 'Gold Bar', 'Ruby'], address: 'Blacksmith', birthday: 'Winter 26'},
+    {id: 14, name : 'Demitrius', marriage: false, topGifts: ['Strawberry', 'Bean Hotpot', 'Ice Cream'], address: '24 Mountain Road', birthday: 'Summer 19'},
+    {id: 15, name : 'Dwarf', marriage: false, topGifts: ['Emerald', 'Jade', 'Ruby'], address: 'Eastern Cave', birthday: 'Summer 22'},
+    {id: 16, name : 'Evelyn', marriage: false, topGifts: ['Beet', 'Tulip', 'Diamond'], address: '1 River Road', birthday: 'Winter 20'},
+    {id: 17, name : 'George', marriage: false, topGifts: ['Leek', 'Fried Mushroom', 'Pearl'], address: '1 River Road', birthday: 'Fall 24'},
+    {id: 18, name : 'Gus', marriage: false, topGifts: ['Orange', 'Escargot', 'Fish Taco'], address: 'The Stardrop Saloon', birthday: 'Summer 8'},
+    {id: 19, name : 'Jas', marriage: false, topGifts: ['Fairy Rose', 'Plum Pudding', 'Pink Cake'], address: 'Marnies Ranch', birthday: 'Summer 4'},
+    {id: 20, name : 'Jodi', marriage: false, topGifts: ['Fried Eel', 'Chocolate Cake', 'Diamond'], address: '1 Willow Lane', birthday: 'Fall 11'},
+    {id: 21, name : 'Kent', marriage: false, topGifts: ['Pearl', 'Fiddlehead Risotto', 'Roasted Hazelnuts'], address: '1 Willow Lane', birthday: 'Spring 4'},
+    {id: 22, name : 'Krobus', marriage: false, topGifts: ['Diamond', 'Void Egg', 'Pumpkin'], address: 'The Sewers', birthday: 'Winter 1'},
+    {id: 23, name : 'Leo', marriage: false, topGifts: ['Poi', 'Duck Feather', 'Mango'], address: 'Ginger Island Forest', birthday: 'Summer 26'},
+    {id: 24, name : 'Lewis', marriage: false, topGifts: ['Hot Pepper', 'Green Tea', 'Glazed Yam'], address: 'Mayors Manor', birthday: 'Spring 7'},
+    {id: 25, name : 'Linus', marriage: false, topGifts: ['Yam', 'Coconut', 'Blueberry Tart'], address: 'Tent', birthday: 'Winter 3'},
+    {id: 26, name : 'Marnie', marriage: false, topGifts: ['Pink Cake', 'Pumpkin Pie', 'Diamond'], address: 'Cindersnap Forest', birthday: 'Fall 18'},
+    {id: 27, name : 'Pam', marriage: false, topGifts: ['Beer', 'Mead', 'Parsnip'], address: 'Trailer', birthday: 'Spring 18'},
+    {id: 28, name : 'Pierre', marriage: false, topGifts: ['Fried Calamari', 'Pearl', 'Rabbits Foot'], address: 'Pierres General Store', birthday: 'Spring 26'},
+    {id: 29, name : 'Robin', marriage: false, topGifts: ['Goat Cheese', 'Peach', 'Spaghetti'], address: '24 Mountain Road', birthday: 'Fall 21'},
+    {id: 30, name : 'Sandy', marriage: false, topGifts: ['Crocus', 'Daffodil', 'Sweet Pea'], address: 'Oasis', birthday: 'Fall 15'},
+    {id: 31, name : 'Vincent', marriage: false, topGifts: ['Grape', 'Snail', 'Pink Cake'], address: '1 Willow Lane', birthday: 'Spring 10'},
+    {id: 32, name : 'Willy', marriage: false, topGifts: ['Diamond', 'Mead', 'Octopus'], address: 'Fish Shop', birthday: 'Summer 24'},
+    {id: 33, name : 'Wizard', marriage: false, topGifts: ['Purple Mushroom', 'Solar Essence', 'Void Essence'], address: 'Wizards Tower', birthday: 'Winter 17'},
+    {id: 34, name : 'Birdie', marriage: false, address: 'Ginder Island Beach Hut'},
+    {id: 35, name : 'Bouncer', marriage: false, address: 'Oasis'},
+    {id: 36, name : 'Gil', marriage: false, address: 'The Adventures Guild',},
+    {id: 37, name : 'Governor', marriage: false},
+    {id: 38, name : 'Gunther', marriage: false, address: 'Museum'},
+    {id: 39, name : 'Henchman', marriage: false, topGifts: 'Void Mayonnaise', address: 'Witches Swamp'},
+    {id: 40, name : 'Morris', marriage: false, address: 'JojaMart'},
+    {id: 41, name : 'Marlon', marriage: false, address: 'The Adventures Guild',},
+    {id: 42, name : 'Mr. Qi', marriage: false, address: 'Casino or Walnut Room',},
+    {id: 43, name : 'Proffessor Snail', marriage: false, address: 'Island Field Office',},
 ]
 
 app.get('/', (req, res) => {
     return res.json('Please enter a specified path');
 })
 
-app.get('/charecter/random', (req, res) =>{
+app.get('/charecters/random', (req, res) =>{
     let i = Math.floor(Math.random() * 44);
-    const charecter = charecters[i];
-    return res.json(charecter);
+    return res.json(charecters[i - 1]);
 })
 
-app.get('/charecter/all', (req, res) =>{
+app.get('/charecters/spouses', (req, res) =>{
+    const candidates = charecters.filter(charecter => charecter.marriage)
+    return res.json(candidates)
+})
+
+app.get('/charecters/random/spouse', (req, res) =>{
+    let i = Math.floor(Math.random() * 12)
+    const candidates = charecters.filter(charecter => charecter.marriage)
+    return res.json(candidates[i - 1])
+})
+
+app.get('/charecters/all', (req, res) =>{
     return res.json(charecters);
 })
 
-app.get('/charecter/:name', (req, res) =>{
+app.get('/charecters/:name', (req, res) =>{
     const {name} = req.params;
     const charecter = charecters.find(charecter => charecter.name.toLowerCase() === name.toLowerCase());
     return res.json(charecter);
 })
 
-app.get('/charecter/:name/gifts', (req, res) =>{
+app.get('/charecters/:name/gifts', (req, res) =>{
     const {name} = req.params;
     const charecter = charecters.find(charecter => charecter.name.toLowerCase() === name.toLowerCase());
     return res.json(charecter.topGifts)
